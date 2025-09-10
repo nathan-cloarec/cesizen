1198bd089872d20eb5db1c876a29e40c0747ffc7
Nathan <nathan.cloarec@viacesi.fr>
2025-09-10 06:11:36 +0200

    chore: preview demo

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0D6VetiIzxg2-TkIO_RQfwuk3dKSpQ80",
-  authDomain: "cesizen-a9b65.firebaseapp.com",
-  projectId: "cesizen-a9b65",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+
   storageBucket: "cesizen-a9b65.appspot.com",
   messagingSenderId: "478608003132",
-  appId: "1:478608003132:web:03d8d3044b9ef0524fb339",
   measurementId: "G-TRGLM6JPJW"
 };
 
